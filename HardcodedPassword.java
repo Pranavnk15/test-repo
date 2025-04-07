6453a819698b60190833b6c5a9d74f4b5301e1f2
Pranav Khandagale <96475101+Pranavnk15@users.noreply.github.com>
2025-04-07 12:32:41 +0530

    Delete HardcodedPassword.java

--- HardcodedPassword.java
+++
@@ -1,8 +0,0 @@
-public class HardcodedPassword {
-    public static void main(String[] args) {
-        String password = "admin123"; // Hardcoded password
-        if (password.equals("admin123")) {
-            System.out.println("Access granted.");
-        }
-    }
-}
