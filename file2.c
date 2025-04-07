f86f46ece726b0984d9a4a45f19586f2b400ffb8
Pranav Khandagale <96475101+Pranavnk15@users.noreply.github.com>
2025-04-07 11:54:25 +0530

    Update and rename file2.c to HardcodedPassword.java

--- file2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char input[100];
-    printf("Enter input: ");
-    gets(input);  // Unsafe function
-    printf(input);  // Format string vulnerability
-    return 0;
-}
